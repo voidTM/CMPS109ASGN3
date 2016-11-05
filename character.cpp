fe7cf0cbca988a90ae5fd4cb2e90d5c044adf1f4
sam song <sasong@ucsc.edu>
2016-11-05 11:47:22 -0700

    mistakes were made?

---
+++ character.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "character.h"
+
+// Constructors
+Character::Character(){
+	value = ' ';
+}
+
+Character::Character(char val){
+	value = val;
+}
+
+//Methods
+
+void Character::initialize(char* token){
+	char val;
+	int t;
+	// check for single quotes at end of quotation
+	//if(val[0] == '\'' && val[length - 1] == '\'')
+	t = sscanf(token, ' %c', &val);
+	if(t == 1)
+		value = val;
+}
+
+Identifier* Character::clone(vector<char*> argv){
+	if (argv.size() > 1){
+		// Throw error due to in proper amount of
+		// values
+		cerr << "Invalid value to Character" << endl;
+		exit(1);
+	}
+
+	char* tok = argv[0];
+
+	Character* chr = new Character();
+	chr->initialize(tok);
+
+	return chr;
+}
+
+// Destructor
+Character::~Character(){}
---
+++ mis_string.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+#include "mis_string.h"
+
+// Constructors
+String::String(){
+	value = new char[256];
+	size = 256;
+}
+
+String::String(string val){
+	strcpy(value, val.c_str());
+	size = val.size();
+	//value = val;
+}
+
+
+String::String(int size){
+	value = new char[size];
+	this->size = size;
+}
+//Methods
+
+void String::initialize(char* token){
+	int t;
+	int length;
+	string val;
+	length = strlen(token);
+
+	// check for double quotes at each end of quotation
+	if(token[0] == '\"' && token[length - 1] == '\"')
+	{
+		t = 1;
+		// copy without first and last value
+		// removes double quotes
+		val = token;
+		val.erase(val.begin());
+		val.erase(val.end());
+	}
+
+
+	if(t == 1)
+		strcpy(value, val.c_str());
+}
+
+Identifier* String::clone(vector<char*> argv){
+	//char* tokSize;
+	char* tokVal;
+	int tokSize = 256;
+	String* chr;
+	if (argv.size() > 2){
+		// Throw error due to in proper amount of
+		// values
+		cerr << "Invalid value to Character" << endl;
+		exit(1);
+	}
+	else if(argv.size() == 2){
+		// check to see if there are 2 arguments for the 
+		// class this would mean first one should be the size
+		// of the string
+		//*tokSize = argv[0];
+		tokVal = argv[1];
+		sscanf(argv[0], " %d", &tokSize); //get sizes
+	}
+	else{
+		// Assume there is only 1 argument
+		tokVal = argv[0];
+		//*chr = new String();
+	}
+
+	chr = new String(tokSize);
+	chr->initialize(tokVal);
+
+	return chr;
+}
+
+// Destructor
+String::~String(){}
